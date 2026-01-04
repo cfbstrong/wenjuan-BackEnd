61650303483c823a62c6afa8906fedd5f33ea4eb
fengbin_chen <1563540102@qq.com>
2026-01-04 17:23:44 +0800

    feature: connect mongodb database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuestionModule } from './question/question.module';
 
 @Module({
-  imports: [QuestionModule],
+  imports: [
+    QuestionModule,
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/wenjuan-BackEndDB'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
