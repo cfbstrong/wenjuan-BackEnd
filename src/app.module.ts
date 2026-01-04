10aaae5381b34eefc420e50f6c1808bb42c49dae
fengbin_chen <1563540102@qq.com>
2026-01-04 20:12:52 +0800

    feature: Decouple configuration information

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuestionModule } from './question/question.module';
@@ -7,7 +8,11 @@ import { QuestionModule } from './question/question.module';
 @Module({
   imports: [
     QuestionModule,
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/wenjuan-BackEndDB'),
+    ConfigModule.forRoot(),
+    // MongooseModule.forRoot('mongodb://127.0.0.1:27017/wenjuan-BackEndDB'),
+    MongooseModule.forRoot(
+      `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`,
+    ),
   ],
   controllers: [AppController],
   providers: [AppService],
