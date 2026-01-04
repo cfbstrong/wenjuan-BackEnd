3c7153f02b54bc9330d71e37ab16604ecdc51b6a
fengbin_chen <1563540102@qq.com>
2026-01-04 22:00:55 +0800

    feature: create schemas data model

--- src/question/question.module.ts
+++ src/question/question.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Question, QuestionSchema } from './schemas/question.schema';
 import { QuestionController } from './question.controller';
 import { QuestionService } from './question.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Question.name, schema: QuestionSchema },
+    ]),
+  ],
   controllers: [QuestionController],
   providers: [QuestionService],
 })
