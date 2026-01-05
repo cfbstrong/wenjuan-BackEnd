273966f9be09b9e436057e0b70bb3f27e14fbd14
fengbin_chen <1563540102@qq.com>
2026-01-05 23:16:45 +0800

    feature: Set up JWT global validation and add a custom public mechanism

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,19 +8,21 @@ import {
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../user/dto/create-user.dto';
-import { AuthGuard } from './auth.guard';
+// import { AuthGuard } from './auth.guard';
+import { Public } from './decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public() // 装饰器，表示这个路由不需要认证
   @Post('login')
   async login(@Body() userInfo: CreateUserDto) {
     const { username, password } = userInfo;
     return await this.authService.signIn(username, password);
   }
 
-  @UseGuards(AuthGuard)
+  // @UseGuards(AuthGuard)
   @Get('profile')
   async getProfile(@Request() req) {
     // request['user'] = payload;
