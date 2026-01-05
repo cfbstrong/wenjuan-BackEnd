72acbaf9e27f370dfb6021ce0d74f820265b1aab
fengbin_chen <1563540102@qq.com>
2026-01-05 23:47:36 +0800

    fix: redirect route

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -4,14 +4,18 @@ import {
   Body,
   HttpException,
   HttpStatus,
+  Redirect,
+  Get,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { Public } from 'src/auth/decorators/public.decorator';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Public()
   @Post('register')
   async createUser(@Body() userDto: CreateUserDto) {
     try {
@@ -19,5 +23,18 @@ export class UserController {
     } catch (err) {
       throw new HttpException(err.message, HttpStatus.BAD_REQUEST);
     }
+  }
+
+  @Get('info')
+  @Redirect('/api/auth/profile', 302)
+  async info() {
+    return;
+  }
+
+  @Public()
+  @Post('login')
+  @Redirect('/api/auth/login', 307)
+  async login() {
+    return;
   }
 }
