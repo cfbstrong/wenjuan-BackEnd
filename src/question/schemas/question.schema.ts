5cde3a6554ce3f5f5a1bd9f59fdf63f455e6bc46
fengbin_chen <1563540102@qq.com>
2026-01-06 08:45:29 +0800

    feature: Improve question schema data model

--- src/question/schemas/question.schema.ts
+++ src/question/schemas/question.schema.ts
@@ -10,6 +10,34 @@ export class Question {
 
   @Prop()
   description: string;
+
+  @Prop()
+  js: string;
+
+  @Prop()
+  css: string;
+
+  @Prop({ default: false })
+  isPublished: boolean;
+
+  @Prop({ default: false })
+  isStar: boolean;
+
+  @Prop({ default: false })
+  isDeleted: boolean;
+
+  @Prop({ required: true })
+  author: string;
+
+  @Prop({ required: true })
+  componentList: {
+    fe_id: string; //组件fe_id需要前端生成
+    type: string; //组件类型
+    title: string; //组件标题
+    isHidden: boolean; //是否隐藏
+    isLocked: boolean; //是否锁定
+    props: object; //组件属性
+  }[];
 }
 
 export const QuestionSchema = SchemaFactory.createForClass(Question);
